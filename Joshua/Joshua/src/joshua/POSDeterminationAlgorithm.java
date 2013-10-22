3564a979e22b80d78e728311fb7b43adb9f6622f
rjd3756 <rjd3756@rit.edu>
2013-10-21 22:06:03 -0400

    comments and a quick fix

--- Joshua/Joshua/src/joshua/POSDeterminationAlgorithm.java
+++ Joshua/Joshua/src/joshua/POSDeterminationAlgorithm.java
@@ -27,12 +27,64 @@ public class POSDeterminationAlgorithm {
         for(Word word : words){
             POS.add(word.stringPOS(word.getPOS()));
         }
+        countPOSIssues(POS);
+        
+        /*i am going to outline the algorithm in comments here
+         * 
+         * unknown words could be handled in multiple ways
+         * the best would be to try to determine them after the bulk
+         * of this algorithm has run
+         * the other would be to immediately stop the algorithm and have the
+         * user provide the basic grammatical information about the word.
+         * The second approach may be what to start with, but in the long run
+         * it seems better to have the program attempt to figure out what words
+         * it does not recognize are.
+         * 
+         * 
+         * first, compound sentences and clauses must be addressed
+         * split compound sentences into two
+         * remove clauses and evaluate with a different algorithm
+         * 
+         * adjectives should be assigned to the word in which they are attached
+         * remember that multiple adjectives and even adverbs could be attached
+         * to a single word. so loop through until all of these can be placed in
+         * other words.
+         * also, prepositions should work like this as well
+         * 
+         * next, if the sentence has no verb and only one possible verb
+         * change that one to a verb
+         * 
+         * if the sentence has no noun and one possible noun, change that
+         * to a noun, but be aware that pronouns could screw with this approach,
+         * it may become even more complicated than this.
+         * 
+         * 
+         */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         //Test Print statement
         System.out.println(POS.toString());
         
         
         //this is just so I can run the program to test
+        //in the future this will hopefully return the sentence structure that
+        //lacks undetermined parts of speech
         return words;
     }
         private void countPOSIssues(ArrayList<String> sentenceStructure){
